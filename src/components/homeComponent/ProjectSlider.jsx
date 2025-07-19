6d6b67a4842fe9f0c5db814b401cefd22f0462ea
Arifa <arifaiffat1995@gmail.com>
2025-07-19 12:26:49 +0530

    recent project section added on home page but to replace the details/content of that section later.

--- src/components/homeComponent/ProjectSlider.jsx
+++ src/components/homeComponent/ProjectSlider.jsx
@@ -1,24 +1,22 @@
-
-
 import Carousel from "react-multi-carousel";
 import "react-multi-carousel/lib/styles.css";
 
 const projectData = [
   {
     title: "Zinio Office",
-    image: "/images/zinio.jpg",
+    image: "/images/project1.png",
   },
   {
     title: "Tourism Malaysia",
-    image: "/images/tourism.jpg",
+    image: "/images/project2.png",
   },
   {
     title: "Truman",
-    image: "/images/truman.jpg",
+    image: "/images/project3.png",
   },
   {
     title: "TechHub",
-    image: "/images/techhub.jpg",
+    image: "/images/project4.png",
   },
 ];
 
@@ -26,48 +24,52 @@ const responsive = {
   desktop: {
     breakpoint: { max: 3000, min: 1024 },
     items: 3,
-    slidesToSlide: 1,
   },
   tablet: {
     breakpoint: { max: 1024, min: 768 },
     items: 2,
-    slidesToSlide: 1,
   },
   mobile: {
     breakpoint: { max: 768, min: 0 },
     items: 1,
-    slidesToSlide: 1,
   },
 };
 
 const ProjectSlider = () => {
   return (
-    <div className="project-slider px-4 py-5 bg-light text-center">
-      <h2 className="mb-4">A Selection of Signature Projects</h2>
-      <Carousel
-        responsive={responsive}
-        infinite
-        autoPlay
-        swipeable
-        draggable
-        showDots
-        dotListClass="custom-dot-list-style"
-        itemClass="carousel-item-padding-40-px"
-      >
-        {projectData.map((project, index) => (
-          <div key={index} className="card border-0 shadow-sm">
-            <img
-              src={project.image}
-              alt={project.title}
-              className="card-img-top"
-              style={{ height: "200px", objectFit: "cover" }}
-            />
-            <div className="card-body">
-              <h5 className="card-title">{project.title}</h5>
+    <div className="proj_slider my-5">
+      
+      <div className="container">
+        <Carousel
+          responsive={responsive}
+          infinite
+          // autoPlay
+          swipeable
+          draggable
+          showDots
+          arrows = {false}
+          dotListClass="custom-dot-list-style text-center"
+          itemClass="carousel-item-padding-40-px px-2"
+        >
+          {projectData.map((project, index) => (
+            <div
+              key={index}
+              className="bg-white shadow-sm rounded-4 overflow-hidden"
+              // style={{ height: "350px" }}
+            >
+              <img
+                src={project.image}
+                alt={project.title}
+                className="w-100"
+                style={{ height: "320px", objectFit: "cover" }}
+              />
+              <div className="p-3">
+                <h5 className="text-center">{project.title}</h5>
+              </div>
             </div>
-          </div>
-        ))}
-      </Carousel>
+          ))}
+        </Carousel>
+      </div>
     </div>
   );
 };
--- src/components/homeComponent/RecentProjectSection.jsx
+++ src/components/homeComponent/RecentProjectSection.jsx
@@ -3,7 +3,12 @@ import ProjectSlider from "./ProjectSlider"
 
 const RecentProjectSection = () => {
   return (
-    <ProjectSlider/>
+    <>
+      <div className="container">
+        <h1 className="cust_h1_heading text-center"><span className="blueColorText">We&apos;ve Been </span> <span className="coloredText">Working On</span></h1>
+      </div>
+      <ProjectSlider />
+    </>
   )
 }
 
