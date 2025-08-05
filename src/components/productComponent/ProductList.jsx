9ee5dc0bbb088a6ddfa94502d1576e577eead078
Arifa <arifaiffat1995@gmail.com>
2025-08-06 00:43:13 +0530

    product page setup

--- src/components/productComponent/ProductList.jsx
+++ src/components/productComponent/ProductList.jsx
@@ -2,45 +2,44 @@
 import { useEffect } from "react";
 import AOS from 'aos';
 import 'aos/dist/aos.css'
-import TernaryButton from "../customButtons/TernaryButton";
+import ProductDetails from "../../data/ProductDetails";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowAltCircleDown } from "@fortawesome/free-solid-svg-icons";
+import { Link } from "react-router-dom";
 
 const ProductList = () => {
 
-      useEffect(() => {
-       AOS.init({duration: 1500});
-        
-      }, [])
+  useEffect(() => {
+    AOS.init({ duration: 1500 });
 
-    const productDetails = [
-        { id: 1, prodName: 'TM-5 : BOD INCUBATOR', img: 'images/product1.png', 
-          prodDesc: 'A BOD Incubator is specialized laboratory equipment used to maintain controlled conditions like temperature and humidity. It is primarily utilized in microbiology, research labs, and industries for testing the oxygen demand of samples over a specific period.' },
-        { id: 2, prodName: 'TM-10 :  FUME HOOD', img: 'images/product2.png', prodDesc: 'A fume hood is a ventilated enclosure in which gases, vapors and fumes are captured and removed from the work area. An exhaust fan situated on the top of the laboratory building pulls air and airborne contaminants through connected ductwork and exhausts them to the atmosphere.' },
-        
-      
-      ];
+  }, [])
+
+
 
 
   return (
     <div className="row">
-          {productDetails.map((product,index) => (
+      {ProductDetails.map((product, index) => (
 
-            <div key={product.id} className="col-12  mb-5 prod_col"  >
-              <div className="d-md-flex align-flex-start prodBox" data-aos='fade-up' data-aos-duration={index*250}>
-                <img src={product.img} className="prod_img" />
-                <div className="eachProdBG p-3 text-white">
-                  <h3 className="mb-4">{product.prodName}</h3>
-                  <p>{product.prodDesc}</p>
-                  <TernaryButton btnLink="#" buttonName="Get a Quote"/>
-                  {/* <a href="pdf/Catalogue-pdf.pdf" target="_blank" className="text-white py-1 px-2 border border-1 rounded my-5 text-decoration-none">Catalogue <FontAwesomeIcon icon={faArrowAltCircleDown} className='ms-2'/></a> */}
-                </div>
-                <div>                
-                <img className="floatLogo" src="images/logo_rndBordr.png" alt="CoLogo" style={{width: '70px'}} />
+        <div key={product.id} className="col-12 col-md-3  mb-5 prod_col"  >
+          <div className="text-center prodBox" data-aos='fade-up' data-aos-duration={index * 250}>
+            <img src={product.img} alt="" className="prod_img" style={{ width: '100%' }} />
+            <div className="eachProdBG my-3 ">
+              <h6 className="mb-3">{product.prodName}</h6>
+              <p>{product.prodDesc}</p>
+              <div className="my-3">
+              <Link to="/contact" target="_blank" className=" py-1 px-2 border border-1 rounded text-decoration-none" style={{width: '100%', display: 'inline-block'}}>Get a quote <FontAwesomeIcon icon={faArrowAltCircleDown} className='ms-2' /></Link>
               </div>
+              <div className="">
+              <a href="pdf/Catalogue-pdf.pdf" target="_blank" rel="noopener noreferrer" className="text-white bg-dark py-1 px-2 border border-1 rounded text-decoration-none" style={{width: '100%', display: 'inline-block'}}>Catalogue <FontAwesomeIcon icon={faArrowAltCircleDown} className='ms-2' /></a>
               </div>
-              
             </div>
-          ))}
+
+          </div>
+
         </div>
+      ))}
+    </div>
   )
 }
 
---
+++ src/data/ProductDetails.jsx
@@ -0,0 +1,18 @@
+const ProductDetails = [
+  {
+    id: 1,
+    prodName: 'TM-5 : BOD INCUBATOR',
+    img: 'images/product1.png',
+    prodDesc:
+      'Maintains temperature and humidity for microbiology and research labs to test oxygen demand.',
+  },
+  {
+    id: 2,
+    prodName: 'TM-10 : FUME HOOD',
+    img: 'images/product2.png',
+    prodDesc:
+      'Captures and removes harmful fumes using ventilation, ensuring lab safety during experiments.',
+  },
+];
+
+export default ProductDetails;
